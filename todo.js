f8df68bb3fc5d6ced1ec3905aaa7846b3e825375
Piyush Raj <148261733+piyushgupta1319@users.noreply.github.com>
2025-03-17 00:37:11 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,11 +1,11 @@
 let todoList = [
     {
       item: 'Buy Milk',
-      dueDate: '4/10/2023'
+      dueDate: '4/03/2025'
     },
     {
       item: 'Go to College',
-      dueDate: '4/10/2023'
+      dueDate: '4/03/2025'
     }
   ];
   
